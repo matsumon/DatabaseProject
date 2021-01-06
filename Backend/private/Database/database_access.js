1ec7273e1dc1cf1c026fef114c9f3aa3e371ac2c
Brian Smith <brian.smith@egov.com>
2021-01-05 16:23:35 -0800

    upgraded logs, added SQL pool library export

--- Backend/private/Database/database_access.js
+++ Backend/private/Database/database_access.js
@@ -1,5 +1,22 @@
 const support = require('../support.js');
-// expose useful functions to js scripts that may need them
-module.exports = {
+const config = require('../../init_config.json');
+var mysql = require('mysql')
 
+support.log("debug", "database_access.js - Configuring Database Connection object with values from config file");
+
+const pool_config = {
+    "connectionLimit": config.db_connectionLimit,
+    "host": config.db_host,
+    "user": config.db_serviceUser,
+    "password": config.db_password,
+    "database": config.db_rootDatabase
 }
+
+support.log("debug", "database_access.js - Establishing Connection to Database with configured arguments");
+
+var pool = mysql.createPool(pool_config);
+
+support.log("debug", "database_access.js - Database Connection ESTABLISHED");
+support.log("info", "database_access.js - Database Connection ESTABLISHED");
+
+module.exports.pool = pool;
--- Backend/private/routes/main_handlers.js
+++ Backend/private/routes/main_handlers.js
@@ -1,4 +1,4 @@
-const support = require('../support.js');
+const support = require('../support.js');   // import support functions
 
 module.exports = function(app){
     //#region main page GET Request Handler
@@ -7,7 +7,21 @@ module.exports = function(app){
         var time = support.getBasicDate(); // Create time stamp for incident
         var response = "HTTP 200 - OK : PONG! - " + time;
         res.send(response);
+
         
+        support.log("debug", "HTTP 200 - OK : PONG sent as a result of user request")
+    });
+    //#endregion
+    //#region API interface JSON PUT handler
+    app.put('/', function(req, res){
+        res.status(200);
+        var time = support.getBasicDate(); // Create time stamp for incident
+        var response = "HTTP 200 - OK : API REQUEST RECEIVED - " + time;
+        res.send(response);
+
+
+        support.log("debug", "HTTP 200 - OK : PONG sent as a result of user request")
+
     });
     //#endregion
   
