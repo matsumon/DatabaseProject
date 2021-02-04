7231e26333f0913f074a155d0bfe950a25bd657e
matsumon <matsumon@oregonstate.edu>
2021-02-04 01:22:25 -0800

    Front end fixes buttons navbar

--- Frontend/react/src/Role.js
+++ Frontend/react/src/Role.js
@@ -1,156 +1,151 @@
-import { Table, Button, Typography, Input, Select, message, Card, InputNumber } from 'antd';
-import {useState} from 'react';
-import {
-  useHistory,
-  useParams
-} from "react-router-dom";
+import { Table, Button, Typography, Input, Select, message, Card } from "antd";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import _ from "lodash";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 function Role() {
-
   const { Column } = Table;
-  const {Option} = Select
+  const { Option } = Select;
   console.log("RENDER");
   const { Title } = Typography;
   const history = useHistory();
-  const [roleTitle,setRoleTitle]=useState("");
-  const [roleUserID,setRoleUserID]=useState("");
+  const [roleTitle, setRoleTitle] = useState("");
+  const [roleUserID, setRoleUserID] = useState("");
   // State variable to trigger a re-render of component
-  const [render,setRender]=useState(false);
-const urlUserID= useParams("userId").userId;
+  const [render, setRender] = useState(false);
 
   /**
    * This data is held in a state variable so that it wont reset every time the component
    * is re-rendered. This data represents the current state of data in sql
    */
-  const [rawData,setRawData]= useState([
-    {id: 1, roleTitle:"Pilot"},
-    {id: 2, roleTitle:"Capitain"},
-    {id: 3, roleTitle:"Officer"}
+  const [rawData, setRawData] = useState([
+    { id: 1, roleTitle: "Pilot" },
+    { id: 2, roleTitle: "Capitain" },
+    { id: 3, roleTitle: "Officer" },
   ]);
-  const userOptions=_.map([1,2,3,4,5,6,7,8,9,10],(element,index)=>{return <Option key={index} value={element} label={element}/>})
-  function handleSelectUserChange(value){
-    console.log(value)
-    setRoleUserID(value)
-    
+  const userOptions = _.map(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    (element, index) => {
+      return <Option key={index} value={element} label={element} />;
+    }
+  );
+  function handleSelectUserChange(value) {
+    console.log(value);
+    setRoleUserID(value);
   }
   /**
-     * This function creates the data packet for antd's table. Because
-     * the table is having issues we have to set deleted rows to null.
-     */
-    function createDataSource(){
-      return _.map(rawData,(object,index)=>{
-        return {
-          key : index,
-          id: object.id,
-          roleTitle:  object.roleTitle,
-          edit: 
-          <div>
-            <Button 
-              onClick={()=>{history.push(`/Sessions/${urlUserID}`)}}
-            >
-              Sessions
-            </Button>
-            <Button 
-              onClick={()=>{history.push(`/Users`)}}
-            >
-              Users
-            </Button>
-            <Button 
-              onClick={()=>{history.push(`/Actions/${urlUserID}`)}}
-            >
-              Available Actions
-            </Button>
-            <Button 
-              onClick={()=>{history.push(`/Credentials/${urlUserID}`)}}
-            >
-              Credentials
-            </Button>
-            </div>
-        }
-      });
+   * This function creates the data packet for antd's table. Because
+   * the table is having issues we have to set deleted rows to null.
+   */
+  function createDataSource() {
+    return _.map(rawData, (object, index) => {
+      return {
+        key: index,
+        id: object.id,
+        roleTitle: object.roleTitle,
+      };
+    });
+  }
+  /**
+   * This function adds users to the table.
+   * Because react only does shallow checks on object, we have to manually
+   * retrigger a render using the render state variable. This isn't
+   * the best programming practice, but oh well.
+   */
+  function addRoleToTable() {
+    if (roleTitle === null || roleTitle.length === 0) {
+      message.error("Role was not filled out");
+      return;
     }
-    /** 
-     * This function adds users to the table.
-     * Because react only does shallow checks on object, we have to manually
-     * retrigger a render using the render state variable. This isn't
-     * the best programming practice, but oh well.
-     */
-    function addRoleToTable (){ 
-      if(roleTitle === null || roleTitle.length === 0){
-        message.error("Role was not filled out");
-        return;
-    }
-      let tempRawData = rawData
-      tempRawData.push({id: "",roleTitle:roleTitle, newUser:roleUserID});
-      setRoleTitle("")
-      setRoleUserID("")
-      setRawData(tempRawData);
-      setRender(!render);
-    }
-    let dataToBeUsed= []
-    dataToBeUsed=createDataSource()
+    let tempRawData = rawData;
+    tempRawData.push({ id: "", roleTitle: roleTitle, newUser: roleUserID });
+    setRoleTitle("");
+    setRoleUserID("");
+    setRawData(tempRawData);
+    setRender(!render);
+  }
+  let dataToBeUsed = [];
+  dataToBeUsed = createDataSource();
   return (
-      <div style={{ width: "100%", height: "100%" }}>
-        <Card style={{margin:"auto", width:"100%"}} 
+    <div style={{ width: "100%", height: "100%" }}>
+      <Card
+        style={{ margin: "auto", width: "100%" }}
         title={
-        <Title style={{margin:"auto", width:"fit-content"}}>Roles</Title>
-        } 
+          <Title style={{ margin: "auto", width: "fit-content" }}>Roles</Title>
+        }
+      >
+        <p>
+          Users can make new roles as long as the role is not blank and is
+          unique. Additionally, users can be tied to roles.
+        </p>
+      </Card>
+      <div>
+        <Button
+          onClick={() => {
+            history.push(`/Sessions`);
+          }}
         >
-          <p>
-            The below roles belong to the user id {`${urlUserID}`}
-          </p>
-          <p>
-            Users can make new roles for any user with a new role name.
-            If a User ID is added a relationship between that user id and that new role will be created. The buttons on 
-            that row will take the user to the new User specified.
-          </p>
-          <p>
-          Users can navigate to the user id: {`${urlUserID}`} sessions, user id: {`${urlUserID}`} actions, 
-          and  user id: {`${urlUserID}`} credentials
-            Alternatively the user can navigate back to the main user table.
-          </p>
-       </Card>
-        <Table style={{width:"auto"}} dataSource={dataToBeUsed}>
-            <Column title="ID" dataIndex="id" key="id" />
-            <Column title="Roles" dataIndex="roleTitle" key="roleTitle" />
-            <Column title="Action" dataIndex="edit" key="edit" />
-        </Table>
-        <Card bordered style={{ width: 300 }}>
-        <div style={{marginBottom:"16px"}}>
-        <Button size="large" onClick={addRoleToTable}>
-          Add Role
+          Sessions
         </Button>
+        <Button
+          onClick={() => {
+            history.push(`/Users`);
+          }}
+        >
+          Users
+        </Button>
+        <Button
+          onClick={() => {
+            history.push(`/Actions`);
+          }}
+        >
+          Available Actions
+        </Button>
+        <Button
+          onClick={() => {
+            history.push(`/Credentials`);
+          }}
+        >
+          Credentials
+        </Button>
+      </div>
+      <Table style={{ width: "auto" }} dataSource={dataToBeUsed}>
+        <Column title="ID" dataIndex="id" key="id" />
+        <Column title="Roles" dataIndex="roleTitle" key="roleTitle" />
+      </Table>
+      <Card bordered style={{ width: 300 }}>
+        <div style={{ marginBottom: "16px" }}>
+          <Button size="large" onClick={addRoleToTable}>
+            Add Role
+          </Button>
         </div>
         <Button style={{ width: "35%" }} disabled>
           Role
-          </Button> 
-        <Input 
-        style={{ width: "55%" }}
-          placeholder ="add role"
+        </Button>
+        <Input
+          style={{ width: "55%" }}
+          placeholder="add role"
           value={roleTitle}
-           onChange={(newValue)=>{setRoleTitle(newValue.currentTarget.value);}} 
-        />    
+          onChange={(newValue) => {
+            setRoleTitle(newValue.currentTarget.value);
+          }}
+        />
         <Button style={{ width: "55%" }} disabled>
           User ID's to Add
-          </Button> 
-          <Select
-          style={{ width: '35%' }}
+        </Button>
+        <Select
+          style={{ width: "35%" }}
           placeholder="User Id"
-          // value={roleUserID}
-          onChange={(value)=>{handleSelectUserChange(value)}}
+          onChange={(value) => {
+            handleSelectUserChange(value);
+          }}
           optionLabelProp="label"
-        > {userOptions}</Select>
-        {/* <InputNumber 
-        min={1}
-        style={{ width: "35%" }}
-          placeholder ="User ID"
-          value={roleUserID}
-           onChange={(newValue)=>{setRoleUserID(newValue);}} 
-        />     */}
-        </Card>
-      </div>
-    );
+        >
+          {userOptions}
+        </Select>
+      </Card>
+    </div>
+  );
 }
 
 export default Role;
--- Frontend/react/src/User.js
+++ Frontend/react/src/User.js
@@ -1,155 +1,167 @@
-import { Table, Button, Typography, Input, Card, DatePicker, message } from 'antd';
-import {useState} from 'react';
 import {
-  useHistory
-} from "react-router-dom";
+  Table,
+  Button,
+  Typography,
+  Input,
+  Card,
+  DatePicker,
+  message,
+} from "antd";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import _ from "lodash";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
 function User() {
   const { Column } = Table;
   console.log("RENDER");
   const { Title } = Typography;
   const history = useHistory();
-  const [username,setUsername]=useState("");
-  const [created,setCreated]=useState("");
-  const [email,setEmail]=useState("");
+  const [username, setUsername] = useState("");
+  const [created, setCreated] = useState("");
+  const [email, setEmail] = useState("");
   // State variable to trigger a re-render of component
-  const [render,setRender]=useState(false);
+  const [render, setRender] = useState(false);
   /**
    * This data is held in a state variable so that it wont reset every time the component
    * is re-rendered. This data represents the current state of data in sql
    */
-  const [rawData,setRawData]= useState([
-    {id: 1, username: "Mike",created:"1/2/21", email:"blahblah@gmail.com"},
-    {id: 2, username: "Lucy",created:"21/2/31", email:"dsfadsa@gmail.com"},
-    {id: 3, username: "Will",created:"1/123/21", email:"asdfdf@gmail.com"}
+  const [rawData, setRawData] = useState([
+    { id: 1, username: "Mike", created: "1/2/21", email: "blahblah@gmail.com" },
+    { id: 2, username: "Lucy", created: "21/2/31", email: "dsfadsa@gmail.com" },
+    { id: 3, username: "Will", created: "1/123/21", email: "asdfdf@gmail.com" },
   ]);
-    /**
-     * This function creates the data packet for antd's table. Because
-     * the table is having issues we have to set deleted rows to null.
-     */
-    function createDataSource(){
-      return _.map(rawData,(object,index)=>{
-        return {
-          key : index,
-          id: object.id,
-          username:  
-                      object.username,
-          created:  
-                      object.created,
-          email:  
-                    object.email,
-          edit: 
-          <div>
-            <Button 
-              onClick={()=>{history.push(`/Sessions/${object.id}`)}}
-            >
-              Sessions
-            </Button>
-            <Button 
-              onClick={()=>{history.push(`/Roles/${object.id}`)}}
-            >
-              Roles
-            </Button>
-            <Button 
-              onClick={()=>{history.push(`/Actions/${object.id}`)}}
-            >
-              Available Actions
-            </Button>
-            <Button 
-              onClick={()=>{history.push(`/Credentials/${object.id}`)}}
-            >
-              Credentials
-            </Button>
-            </div>
-        }
-      })
+  /**
+   * This function creates the data packet for antd's table. Because
+   * the table is having issues we have to set deleted rows to null.
+   */
+  function createDataSource() {
+    return _.map(rawData, (object, index) => {
+      return {
+        key: index,
+        id: object.id,
+        username: object.username,
+        created: object.created,
+        email: object.email,
+      };
+    });
+  }
+  /**
+   * This function adds users to the table.
+   * Because react only does shallow checks on object, we have to manually
+   * retrigger a render using the render state variable. This isn't
+   * the best programming practice, but oh well.
+   */
+  function addUserToTable() {
+    if (username === null || username.length === 0 || created.length === 0) {
+      message.error("Username or Date was not filled out");
+      return;
     }
-    /** 
-     * This function adds users to the table.
-     * Because react only does shallow checks on object, we have to manually
-     * retrigger a render using the render state variable. This isn't
-     * the best programming practice, but oh well.
-     */
-    function addUserToTable (){ 
-      if(
-        username === null 
-        || username.length === 0 
-        || created.length === 0 
-        ){
-          message.error("Username or Date was not filled out");
-          return;
-      }
-      let tempRawData = rawData
-      tempRawData.push({id: "",username: username,created:created, email:email});
-      setUsername("")
-      console.log("CREATED",created)
-      setEmail("")
-      setRawData(tempRawData);
-      setRender(!render);
-    }
-    let dataToBeUsed= []
-    dataToBeUsed=createDataSource()
+    let tempRawData = rawData;
+    tempRawData.push({
+      id: "",
+      username: username,
+      created: created,
+      email: email,
+    });
+    setUsername("");
+    setEmail("");
+    setRawData(tempRawData);
+    setRender(!render);
+  }
+  let dataToBeUsed = [];
+  dataToBeUsed = createDataSource();
   return (
-      <div style={{ width: "100%", height: "100%" }}>
-        <Card style={{margin:"auto", width:"100%"}} 
+    <div style={{ width: "100%", height: "100%" }}>
+      <Card
+        style={{ margin: "auto", width: "100%" }}
         title={
-        <Title style={{margin:"auto", width:"fit-content"}}>Users</Title>
-        } 
-        >
-          <p>
-            Users can add users with a username, created date, and optional email
-          </p>
-          <p>
-            Users can navigate to the users sessions, roles, available actions, and credentials.
-          </p>
-       </Card>
-        <Table style={{width:"auto"}} dataSource={dataToBeUsed}>
-            <Column title="ID" dataIndex="id" key="id" />
-            <Column title="Username" dataIndex="username" key="username" />
-            <Column title="Created" dataIndex="created" key="created" />
-            <Column title="Email" dataIndex="email" key="email" />
-            <Column title="Action" dataIndex="edit" key="edit" />
-        </Table>
-        <Card bordered style={{ width: 300 }}>
-          <div style={{marginBottom:"16px"}}>
-            <Button size="large" onClick={addUserToTable}>
+          <Title style={{ margin: "auto", width: "fit-content" }}>Users</Title>
+        }
+      >
+        <p>
+          Users can add users with a mandatory username, mandatory created date,
+          and optional email
+        </p>
+        <p>
+          Users can navigate to the session, role, action, and credential table.
+        </p>
+      </Card>
+      <Button
+        onClick={() => {
+          history.push(`/Sessions`);
+        }}
+      >
+        Sessions
+      </Button>
+      <Button
+        onClick={() => {
+          history.push(`/Roles`);
+        }}
+      >
+        Roles
+      </Button>
+      <Button
+        onClick={() => {
+          history.push(`/Actions`);
+        }}
+      >
+        Available Actions
+      </Button>
+      <Button
+        onClick={() => {
+          history.push(`/Credentials`);
+        }}
+      >
+        Credentials
+      </Button>
+      <Table style={{ width: "auto" }} dataSource={dataToBeUsed}>
+        <Column title="ID" dataIndex="id" key="id" />
+        <Column title="Username" dataIndex="username" key="username" />
+        <Column title="Created" dataIndex="created" key="created" />
+        <Column title="Email" dataIndex="email" key="email" />
+      </Table>
+      <Card bordered style={{ width: 300 }}>
+        <div style={{ marginBottom: "16px" }}>
+          <Button size="large" onClick={addUserToTable}>
             Add User
           </Button>
-          </div>
+        </div>
         <Button style={{ width: "35%" }} disabled>
           Username
-          </Button> 
-        <Input 
-        style={{ width: "55%" }}
-          placeholder ="Username"
+        </Button>
+        <Input
+          style={{ width: "55%" }}
+          placeholder="Username"
           value={username}
-           onChange={(newValue)=>{setUsername(newValue.currentTarget.value);}} 
-        />    
-          <Button 
-          style={{ width: "45%" }}
-          disabled>
-          Created Date
-          </Button>
-        <DatePicker
-        style={{ width: "55%" }}
-        placeholder="Created Date"
-          onChange={(date,dateString)=>{setCreated(dateString); }}  
+          onChange={(newValue) => {
+            setUsername(newValue.currentTarget.value);
+          }}
         />
-          <Button 
-          style={{ width: "35%" }}disabled>
+        <Button style={{ width: "45%" }} disabled>
+          Created Date
+        </Button>
+        <DatePicker
+          style={{ width: "55%" }}
+          placeholder="Created Date"
+          onChange={(date, dateString) => {
+            setCreated(dateString);
+          }}
+        />
+        <Button style={{ width: "35%" }} disabled>
           Email
-          </Button>
-        <Input 
-        style={{ width: "65%" }}
-        placeholder ="Email"
-        value={email}
-           onChange={(newValue)=>{setEmail(newValue.currentTarget.value);}} 
-          />
-        </Card>
-      </div>
-    );
+        </Button>
+        <Input
+          style={{ width: "65%" }}
+          placeholder="Email"
+          value={email}
+          onChange={(newValue) => {
+            setEmail(newValue.currentTarget.value);
+          }}
+        />
+      </Card>
+    </div>
+  );
 }
 
 export default User;
