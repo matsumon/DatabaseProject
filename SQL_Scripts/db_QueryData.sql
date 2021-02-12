3d2e5326598e6d473c948c5111d9199ed92661c3
Brian <code-inquries@sfti.in>
2021-02-11 21:03:38 -0800

    updated queries list

--- SQL_Scripts/db_QueryData.sql
+++ SQL_Scripts/db_QueryData.sql
@@ -31,3 +31,34 @@ SELECT * FROM `user_to_role`;
 SELECT FROM SESSION
 */
 SELECT * FROM `session`;
+
+/*
+SELECT FROM ACTION TABLE
+*/
+
+SELECT action.id, action.action_name, role.id AS roleID 
+FROM action JOIN role_to_action ON action.id = role_to_action.action_id
+    JOIN role ON role.id = role_to_action.role_id;
+
+/*
+insert into role
+*/
+
+-- Insert new role into role table
+INSERT INTO role (role_title)
+VALUES(:roleTitleInput)
+-- roleTitleInput will be a varchar Ex) 'Pilot'
+-- Insert new relationship between user and role
+INSERT INTO `user_to_role`(`user_id`,`role`)
+VALUES (:userIDInput,:roleIDInput)
+-- userIDInput will be a integer Ex) 1
+-- roleIDInput will be a integer Ex) 2
+
+/*
+Creation Aciton
+*/
+
+INSERT into action (action_name) VALUES(
+"ACTION"
+)
+
