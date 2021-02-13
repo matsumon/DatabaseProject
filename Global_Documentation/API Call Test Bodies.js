578814bd5e969f9251818ceade71357f29ba222a
Brian <code-inquries@sfti.in>
2021-02-13 15:05:45 -0800

    updated API test call bodies to reflect

--- Global_Documentation/API Call Test Bodies.js
+++ Global_Documentation/API Call Test Bodies.js
@@ -100,7 +100,11 @@ UPDATE_CREDENTIALS_REQ = {
     "username": "test_user00",
     "token": "d7727ef8f9b18177f91fec2dd57afafaa21a041de61391e684f20d45b70cb947",
     "operation_name": "UPDATE_CRED",
-    "task_data": {}
+    "task_data": {
+        "id": "",
+        "enabled": "",
+        "exp_date": ""
+    }
 }
 
 
