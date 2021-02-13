883432bfa201a575a3b3290bffca8c38fdeaccea
matsumon <matsumon@oregonstate.edu>
2021-02-13 09:46:07 -0800

    complted queries

--- SQL_Scripts/db_QueryData.sql
+++ SQL_Scripts/db_QueryData.sql
@@ -1,64 +1,98 @@
-/*
-This contians basic select queries to access each database item as a reference for future development
-*/
+-- This contains the basic SQL queries
 
-/*
-SELECT FROM USER
-*/
+-- Return all rows from the users table
 SELECT * FROM `user`;
 
-/*
-SELECT FROM CREDENTIAL
-*/
+-- Insert a new user into the user table
+INSERT INTO user (username, email, created_at)
+VALUES(:username_user_input, :email_user_input, :created_at_user_input);
+-- username_user_input, are user defined variables Ex) 'Sally'
+-- email_user_input, are user defined variables Ex) 'ranndomemail@email.com'
+-- created_at_user_input are user defined variables Ex) '20-12-06 00:00:00'
+
+-- Return all rows from the credentials table
 SELECT * FROM `credential`;
 
-/*
-SELECT FROM ROLE
-*/
-SELECT * FROM `role`;
+-- Insert a new credential into the credential table
+INSERT INTO credential (hash,exp_date,created_date,enabled,user_id)
+VALUES(:hash_user_input,:exp_date_user_input,:created_date_user_input,:enabled_user_input,:user_id_user_input);
+-- hash_user_input are user defined variables Ex)"123123fdsasdf21"
+-- exp_date_user_input are user defined variables Ex) '20-12-06 00:00:00'
+-- created_date_user_input are user defined variables Ex)'20-12-06 00:00:00'
+-- enabled_user_input are user defined variables Ex) 1
+-- user_id_user_input are user defined variables Ex) 1
 
-/*
-SELECT USER_TO_CREDENTIAL ASSIGNMENT FROM JOIN TABLE
-*/
-SELECT * FROM `user_to_cred`;
-
-/*
-SELECT USER_TO_ROLE ASSIGNMENT FROM JOIN TABLE
-*/
+-- Return all rows from the user_to_role table
 SELECT * FROM `user_to_role`;
 
-/*
-SELECT FROM SESSION
-*/
+-- Return all rows from the role_to_action table
+SELECT * FROM `role_to_action`;
+
+-- Return all rows from the sessions table
 SELECT * FROM `session`;
 
-/*
-SELECT FROM ACTION TABLE
-*/
+-- Create session in session table
+INSERT INTO session (created_at, exp_date, token, user_id, user_req_date)
+VALUES(:created_at_User_Input, :exp_date_User_Input, :token_User_Input, :user_id_User_Input, :user_req_date_User_Input);
+--  created_at_User_Input is the user defined input. Ex) '20-12-06 00:00:00'
+--  exp_date_User_Input is the user defined input. Ex) '20-12-06 00:00:00'
+--  token_User_Input is the user defined input. Ex) "asdf;lsadkj"
+--  user_id_User_Input is the user defined input. Ex) 1
+--  user_req_date_User_Input is the user defined input. Ex) '20-12-06 00:00:00'
 
+-- Display all of the action columns and the role.id column
 SELECT action.id, action.action_name, role.id AS roleID 
 FROM action JOIN role_to_action ON action.id = role_to_action.action_id
     JOIN role ON role.id = role_to_action.role_id;
 
-/*
-insert into role
-*/
-
 -- Insert new role into role table
 INSERT INTO role (role_title)
-VALUES(:roleTitleInput)
+VALUES(:roleTitleInput);
 -- roleTitleInput will be a varchar Ex) 'Pilot'
--- Insert new relationship between user and role
-INSERT INTO `user_to_role`(`user_id`,`role`)
+
+-- Return all the columns from the roles table and the associated relationship with the
+-- user table via the user id
+SELECT role.id, role.role_title, user.id as userID
+FROM role JOIN user_to_role ON role.id = user_to_role.role_id
+    JOIN user ON user.id = user_to_role.user_id;
+
+-- Creating User to role relationship
+INSERT INTO `user_to_role`(`user_id`,`role_id`)
 VALUES (:userIDInput,:roleIDInput)
 -- userIDInput will be a integer Ex) 1
 -- roleIDInput will be a integer Ex) 2
 
-/*
-Creation Aciton
-*/
+-- Creating Action
+INSERT into action (action_name) 
+VALUES(:userActionInput);
+-- userActionInput will be a string Ex) 'SAVE'
+-- Insert new relationship inbetween role and action
 
-INSERT into action (action_name) VALUES(
-"ACTION"
-)
+-- Creating role to action relationship
+INSERT INTO role_to_action (role_id, action_id)
+VALUES(:userRoleIDInput,:userActionIDInput);
+-- userRoleIDInput will be a integer Ex) 1
+-- userActionIDInput will be a integer Ex) 2
 
+-- Deletes a row from the action table based on ID. Deletes the intersection table entries
+-- first and then the entry in the action table.
+DELETE FROM role_to_action
+where(role_to_action.action_id = :actionID_user_input);
+DELETE FROM action
+WHERE(action.id = :actionID_user_input);
+-- actionID_user_input is user defined input Ex) 1
+
+-- Update a action row with different values
+UPDATE action
+SET action_name = :actionName_user_input
+WHERE action.id = :actionID_user_input;
+-- :actionName_user_input are user defined variables Ex) "EDIT"
+-- :actionID_user_input are user defined variables Ex) 1
+
+-- Filters all actions by action ids and action names and returns all action columns
+SELECT * 
+FROM action
+WHERE action.id IN :actionIDArray_user_input 
+    AND action.action_name = :actionName_user_input;
+-- :actionIDArray_user_input are user defined variables Ex) (1,2,3)
+-- :actionName_user_input are user defined variables Ex) "SAVE"
--- SQL_Scripts/db_init.sql
+++ SQL_Scripts/db_init.sql
@@ -59,3 +59,170 @@ ALTER TABLE `session` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 ALTER TABLE `user_to_role` ADD FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
 
 ALTER TABLE `user_to_role` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+/*
+This script should fill our databases with some base data for use in testing and general minipulation
+*/
+
+/*
+Create Three Test Users
+*/
+INSERT INTO `user`
+(`id`,
+`username`,
+`created_at`,
+`email`)
+VALUES
+( 1 ,
+'test_user00',
+current_timestamp(),
+'test_user00@mock_data.dev'),
+( 2 ,
+'test_user01',
+current_timestamp(),
+'test_user01@mock_data.dev'),
+( 3 ,
+'test_user02',
+current_timestamp(),
+'test_user02@mock_data.dev');
+
+
+/*
+Create four test crednetials
+*/
+
+INSERT INTO `credential`
+(
+`id`,
+`hash`,
+`exp_date`,
+`created_date`,
+`enabled`,
+`user_id`
+)
+VALUES
+(1,
+'',
+CURRENT_TIMESTAMP + INTERVAL 1 YEAR,
+CURRENT_TIMESTAMP,
+1,
+1),
+(2,
+'',
+CURRENT_TIMESTAMP + INTERVAL 1 YEAR,
+CURRENT_TIMESTAMP,
+0,
+2
+),
+(3,
+'',
+CURRENT_TIMESTAMP + INTERVAL 1 YEAR,
+CURRENT_TIMESTAMP,
+1,
+3),
+(4,
+'',
+CURRENT_TIMESTAMP + INTERVAL 1 YEAR,
+CURRENT_TIMESTAMP,
+1,
+2);
+
+
+
+/*
+Create Four Types of ROLES
+*/
+
+INSERT INTO `role`
+(`role_title`)
+VALUES
+('USER'),
+('AUTHENTICATING_SERVICE'),
+('SUPER_USER'),
+('DISABLED USER');
+
+INSERT INTO `action`
+(`action_name`)
+VALUES
+('EDIT'),
+('REDO'),
+('DELETE'),
+('CREATE');
+
+/*
+Assign Our Three Users to their Approprate ROLES
+*/
+
+INSERT INTO `user_to_role`
+(`user_id`,
+`role_id`)
+VALUES
+(1,1),
+(2,2),
+(3,3),
+(1,2),
+(1,3);
+
+
+/*
+Create some example sessions
+*/
+
+INSERT INTO `session`
+(`id`,
+`user_id`,
+`token`,
+`exp_date`,
+`user_req_date`,
+`created_at`)
+VALUES
+(1,
+1,
+'-NULL TOKEN - k',
+CURRENT_TIMESTAMP + INTERVAL 15 MINUTE,
+CURRENT_TIMESTAMP - INTERVAL 10 MINUTE,
+CURRENT_TIMESTAMP),
+(2,
+2,
+'-NULL TOKEN - kgh4',
+CURRENT_TIMESTAMP + INTERVAL 15 MINUTE,
+CURRENT_TIMESTAMP - INTERVAL 10 MINUTE,
+CURRENT_TIMESTAMP),
+(3,
+3,
+'-NULL TOKEN - k7hf',
+CURRENT_TIMESTAMP + INTERVAL 15 MINUTE,
+CURRENT_TIMESTAMP - INTERVAL 10 MINUTE,
+CURRENT_TIMESTAMP),
+(4,
+2,
+'-NULL TOKEN - Ka1h',
+CURRENT_TIMESTAMP + INTERVAL 15 MINUTE,
+CURRENT_TIMESTAMP - INTERVAL 10 MINUTE,
+CURRENT_TIMESTAMP),
+(5,
+1,
+'-NULL TOKEN - K1sl',
+CURRENT_TIMESTAMP + INTERVAL 15 MINUTE,
+CURRENT_TIMESTAMP - INTERVAL 10 MINUTE,
+CURRENT_TIMESTAMP);
+
+INSERT INTO `role_to_action`(
+    `action_id`,
+    `role_id`
+)
+VALUES(
+    1,1
+),
+(
+    2,2
+),
+(
+    3,3
+),
+(
+    1,2
+),
+(
+    1,3
+);
